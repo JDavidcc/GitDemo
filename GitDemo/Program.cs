5b3034904869b15c2460ffbb959fb70f77b3cd6d
David Cervantes <jesus_d_cc@hotmail.com>
2023-02-11 09:31:45 -0700

    Enviando commit desde visual studio

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -10,7 +10,7 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo desde Git");
+            Console.WriteLine("Pruba desde visual studio");
         }
     }
 }
